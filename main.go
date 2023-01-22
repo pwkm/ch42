7e709c5e51322caef3dc40cf9890d77ebbc20d40
Peter Winkelmans <peter.winkelmans@sennit.be>
2023-01-22 16:35:51 +0100

    Initial Setup

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+
+	r := httprouter.New()
+	r.GET("/", index)
+	r.GET("/user/:id", getUser)
+	http.ListenAndServe("localhost:8080", r)
+
+}
+
+// ---------------------------------
+// Standard index  URL /
+// ---------------------------------
+func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	s := `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	<meta charset="UTF-8">
+	<title>Index</title>
+	</head>
+	<body>
+	<a href="/user/9872309847">GO TO: http://localhost:8080/user/9872309847</a>
+	</body>
+	</html>
+	`
+	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write(w, []byte(s))
+
+}
+
+// --------------------------------------------
+// GET USER
+// URL /user/:id
+// --------------------------------------------
+func getUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	u := models.User{
+		Name:   "James Bond",
+		Gender: "male",
+		Age:    32,
+		Id:     p.ByName("id"),
+	}
+
+	uj, err := json.Marshal((u))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "Application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s\n", uj)
+}
